b63ee80f50beffc0d9e5f5c8b099c5834ab25940
leo0liu <leo0liu@foxmail.com>
2017-07-01 00:48:38 +0800

    add

---
+++ CameraPosition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour {
+
+    //设置父物体
+    GameObject globalMgr;
+
+    //开始时注视的位置
+    GameObject goalKeeper;
+
+    //是否保持注视
+    bool isLookAt = true;
+
+    void Awake()
+    {
+        globalMgr = GameObject.FindWithTag("GlobalMgr");
+        goalKeeper = GameObject.FindWithTag("GoalKeeper");
+    }
+
+    void Start()
+    {
+      //  Debug.Log("333");
+      //  transform.LookAt(goalKeeper.transform.position);
+    }
+
+    void Update()
+    {
+        //游戏一开始看着守门员
+        //if (isLookAt) {
+        //    transform.LookAt(goalKeeper.transform.position);
+        //    isLookAt = false;
+        //}
+    }
+
+    //改变自己的父物体
+    public void ChangeParent()
+    {
+        transform.SetParent(globalMgr.transform);
+    }
+}
